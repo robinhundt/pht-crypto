79502ebb8060345d90b33fd6b7532d954a580853
Robin Hundt <24554122+robinhundt@users.noreply.github.com>
2021-10-28 18:09:13 +0200

    Implement Partial-/Ord

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use rug::Integer;
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 
 pub mod paillier;
 mod rand;
@@ -13,7 +14,7 @@ pub struct Ciphertext {
     val: Integer,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Plaintext {
     #[serde(with = "crate::util::serde_integer")]
     val: Integer,
@@ -33,12 +34,18 @@ macro_rules! impl_from {
     };
 }
 
-macro_rules! impl_partial_eq_plaintext {
-    ($($eq:ty)+) => {
+macro_rules! impl_partial_eq_ord_plaintext {
+    ($($rhs:ty)+) => {
         $(
-            impl PartialEq<$eq> for Plaintext {
-                fn eq(&self, other: &$eq) -> bool {
+            impl PartialEq<$rhs> for Plaintext {
+                fn eq(&self, other: &$rhs) -> bool {
                     self.val.eq(other)
+                }
+            }
+
+            impl PartialOrd<$rhs> for Plaintext {
+                fn partial_cmp(&self, other: &$rhs) -> Option<Ordering> {
+                    self.val.partial_cmp(other)
                 }
             }
         )+
@@ -48,7 +55,7 @@ macro_rules! impl_partial_eq_plaintext {
 // Damn coherence and lack of specialisation...
 impl_from!(Ciphertext; bool i128 i16 i32 i64 i8 isize u128 u16 u32 u64 u8 usize Integer &Integer);
 impl_from!(Plaintext; bool i128 i16 i32 i64 i8 isize u128 u16 u32 u64 u8 usize Integer &Integer);
-impl_partial_eq_plaintext!(f32 f64 i128 i16 i32 i64 i8 isize u128 u16 u32 u64 u8 usize Integer);
+impl_partial_eq_ord_plaintext!(f32 f64 i128 i16 i32 i64 i8 isize u128 u16 u32 u64 u8 usize Integer);
 
 impl From<Ciphertext> for Integer {
     fn from(c: Ciphertext) -> Self {
